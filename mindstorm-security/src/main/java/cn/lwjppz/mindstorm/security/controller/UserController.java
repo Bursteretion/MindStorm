8d2990f24b37ac5c83d8a3bf893b12bb1b7ae12e
Bursteretion <13850801092@163.com>
2021-05-10 18:30:43 +0800

    集成Gateway网关并添加Knife4j在线API文档

--- mindstorm-security/src/main/java/cn/lwjppz/mindstorm/security/controller/UserController.java
+++ mindstorm-security/src/main/java/cn/lwjppz/mindstorm/security/controller/UserController.java
@@ -2,6 +2,7 @@ package cn.lwjppz.mindstorm.security.controller;
 
 
 import cn.lwjppz.mindstorm.common.core.support.CommonResult;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author lwj
  * @since 2021-05-09
  */
+@Api(tags = "用户控制器")
 @RestController
 @RequestMapping("/security/user")
 public class UserController {
@@ -27,4 +29,3 @@ public class UserController {
     }
 
 }
-
