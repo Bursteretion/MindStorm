828a1e9d8ee7630e2943a9b14ed2c7845695d13d
Bursteretion <13850801092@163.com>
2021-06-05 17:34:38 +0800

    新增知识点业务接口，升级项目jdk为jdk11

--- mindstorm-modules/mindstorm-knowledge/src/main/java/cn/lwjppz/mindstorm/knowledge/service/IdeaService.java
+++ mindstorm-modules/mindstorm-knowledge/src/main/java/cn/lwjppz/mindstorm/knowledge/service/IdeaService.java
@@ -1,7 +1,11 @@
 package cn.lwjppz.mindstorm.knowledge.service;
 
+import cn.lwjppz.mindstorm.knowledge.model.dto.idea.IdeaDTO;
 import cn.lwjppz.mindstorm.knowledge.model.entity.Idea;
+import cn.lwjppz.mindstorm.knowledge.model.vo.idea.IdeaVO;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,6 +17,59 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IdeaService extends IService<Idea> {
 
+    /**
+     * 获取所有知识点
+     *
+     * @return 知识点列表
+     */
+    List<IdeaDTO> getIdeas();
 
+    /**
+     * 根据知识点Id获取知识点信息
+     *
+     * @param ideaId 知识点Id
+     * @return 该Id对应的知识点
+     */
+    Idea getIdea(String ideaId);
+
+    /**
+     * 根据pid获取知识点信息
+     *
+     * @param ideaPid pid
+     * @return 知识点信息（包含所有pid等于该pid的知识点）
+     */
+    IdeaDTO getIdeaByPid(String ideaPid);
+
+    /**
+     * 新增知识点
+     *
+     * @param ideaVO 知识点信息
+     * @return 该知识点
+     */
+    Idea insertIdea(IdeaVO ideaVO);
+
+    /**
+     * 更新知识点信息
+     *
+     * @param ideaVO 知识点信息
+     * @return 知识点信息
+     */
+    Idea updateIdea(IdeaVO ideaVO);
+
+    /**
+     * 根据知识点Id删除该知识点
+     *
+     * @param ideaId 知识点Id
+     * @return 是否删除成功
+     */
+    boolean deleteIdea(String ideaId);
+
+    /**
+     * 批量删除知识点
+     *
+     * @param ideaIds 知识点Id列表
+     * @return 是否删除成功
+     */
+    boolean batchDeleteIdea(List<String> ideaIds);
 
 }
--- mindstorm-modules/mindstorm-knowledge/src/main/java/cn/lwjppz/mindstorm/knowledge/service/impl/IdeaServiceImpl.java
+++ mindstorm-modules/mindstorm-knowledge/src/main/java/cn/lwjppz/mindstorm/knowledge/service/impl/IdeaServiceImpl.java
@@ -1,10 +1,17 @@
 package cn.lwjppz.mindstorm.knowledge.service.impl;
 
+import cn.lwjppz.mindstorm.common.core.enums.ResultStatus;
+import cn.lwjppz.mindstorm.common.core.exception.EntityNotFoundException;
+import cn.lwjppz.mindstorm.common.core.utils.StringUtils;
+import cn.lwjppz.mindstorm.knowledge.model.dto.idea.IdeaDTO;
 import cn.lwjppz.mindstorm.knowledge.model.entity.Idea;
 import cn.lwjppz.mindstorm.knowledge.mapper.IdeaMapper;
+import cn.lwjppz.mindstorm.knowledge.model.vo.idea.IdeaVO;
 import cn.lwjppz.mindstorm.knowledge.service.IdeaService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +24,47 @@ import org.springframework.stereotype.Service;
 @Service
 public class IdeaServiceImpl extends ServiceImpl<IdeaMapper, Idea> implements IdeaService {
 
+    @Override
+    public List<IdeaDTO> getIdeas() {
+        return null;
+    }
+
+    @Override
+    public Idea getIdea(String ideaId) {
+        return null;
+    }
+
+    @Override
+    public IdeaDTO getIdeaByPid(String ideaPid) {
+        return null;
+    }
+
+    @Override
+    public Idea insertIdea(IdeaVO ideaVO) {
+        return null;
+    }
+
+    @Override
+    public Idea updateIdea(IdeaVO ideaVO) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteIdea(String ideaId) {
+        if (StringUtils.isNotEmpty(ideaId)) {
+            Idea idea = getIdea(ideaId);
+            if (null == idea) {
+                throw new EntityNotFoundException(ResultStatus.NOT_FOUND);
+            }
+            baseMapper.deleteById(ideaId);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean batchDeleteIdea(List<String> ideaIds) {
+        return ideaIds.stream()
+                .filter(StringUtils::isNotEmpty)
+                .allMatch(this::deleteIdea);
+    }
 }
